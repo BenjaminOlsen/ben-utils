4531c434f51b40a4e3fe196bbdd6ba0c9427e4f0
Benjamin Olsen <thelarrys@gmail.com>
2023-07-03 20:31:25 +0200

    make dirs

--- make_spec_dataset_large.py
+++ make_spec_dataset_large.py
@@ -1,3 +1,4 @@
+import os
 import utils
 import musdb
 from pathlib import Path
@@ -11,6 +12,19 @@ SAVE_DATAPATH = Path('/WHERE/YOU/WANT/TO/SAVE/THE/SPECTROGRAMS/')
 TEST_PATH = SAVE_DATAPATH / 'test'
 TRAIN_PATH = SAVE_DATAPATH / 'train'
 
+if not os.path.exists(TEST_PATH):
+    os.makedirs(TEST_PATH)
+    print(f"made {TEST_PATH}")
+else:
+    print(f"{TEST_PATH} already exists")
+
+if not os.path.exists(TRAIN_PATH):
+    os.makedirs(TRAIN_PATH)
+    print(f"made {TRAIN_PATH}")
+else:
+    print(f"{TRAIN_PATH} already exists")
+
+    
 t1 = timer()
 utils.save_musdb_spectrograms(musdb_test, save_dir=TEST_PATH, spec_len_in_s=5.0, n_fft=2048, win_length=2047, spec_dimension=(1024,1024), power=None)
 t2 = timer()
